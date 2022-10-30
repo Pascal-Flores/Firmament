7974f618dfd4e536ef631fca1e7d379bb1ab5238
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-10-30 13:42:34 +0100

    added function that loads a url stored inside a .url file and exception

---
+++ src/modules/URLUtils.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "original-fs"
+import {extname} from "path"
+
+export function getURLFromFile(filePath : string) : string | never {
+    if (extname(filePath) === ".url") {
+        let fileContent : string = readFileSync(filePath, 'utf-8');
+        return fileContent.substring(fileContent.indexOf('URL=')+4);
+    }
+    else   
+        throw new NotAURLFileError();
+}
+
+export class NotAURLFileError extends Error {
+    public constructor(errorMessage ?: string) {
+        if (errorMessage)
+            super(errorMessage);
+        else    
+            super();
+    }
+}
