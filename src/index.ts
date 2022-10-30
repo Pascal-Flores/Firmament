22cc389233fa7beb584854eefabe88884e401f65
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-10-30 16:45:59 +0100

    at startup, app now loads wallpaper stored in config file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import {app, ipcMain, BrowserWindow, screen, Screen, Display} from "electron";
+import { homedir } from 'os'
+import { UserConfiguration } from "./modules/UserConfiguration.class";
 import { Wallpaper, WallpaperType } from "./modules/Wallpaper.class";
 import { WallpaperWindow } from "./modules/WallpaperWindow.class";
 
+process.env.CONFIG_DIRECTORY = `${homedir()}/.firmament`;
+
 function initApp() : void {
-    let mainWallpaper : WallpaperWindow = new WallpaperWindow(screen.getPrimaryDisplay(), new Wallpaper('/home/pascal/.firmament/wallpapers/default/default_wallpaper.html', WallpaperType.HTML)); 
+    try {
+        UserConfiguration.loadUserConfiguration()
+        console.log(UserConfiguration.content.currentWallpaper)
+    }
+    catch (error){
+        console.log(`Fin frérot, tu es cringe : ${error}`);
+        app.quit();
+    }
+
+    let currentWallpaper : Wallpaper = new Wallpaper(UserConfiguration.content['currentWallpaper'], WallpaperType.HTML)
+    let mainWallpaper : WallpaperWindow = new WallpaperWindow(screen.getPrimaryDisplay(), currentWallpaper); 
     
 }
 
