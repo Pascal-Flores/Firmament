a42fa1b0572046d3db20d941f2275269f504dcbd
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-10-30 22:46:37 +0100

    added method for sanitizing user config directory

--- src/modules/UserConfiguration.class.ts
+++ src/modules/UserConfiguration.class.ts
@@ -1,5 +1,5 @@
-import { PathLike } from "original-fs";
-import {resolve} from "path"
+import { cpSync, existsSync, mkdirSync, PathLike } from "original-fs";
+import {resolve, dirname} from "path"
 import { parseJSONFromFile, writeJSONToFile } from "./JSONFromTo";
 
 export class UserConfiguration {
@@ -26,6 +26,24 @@ export class UserConfiguration {
         catch (error) {
             console.log(`Error during configuration save :\n\t${error}\nUser Configuration won't be saved!`)
         }
+    }
+
+    public static sanitizeUserConfigurationDirectory() : void {
+        if (!process.env.CONFIG_DIRECTORY)
+            throw new Error('Attempt to load configuration before config directory path definition!');
+        
+        let cd = process.env.CONFIG_DIRECTORY;
+
+        if (!existsSync(`${cd}/wallpapers/default`))
+            mkdirSync(`${cd}/wallpapers/default`, {recursive : true});
+        if (!existsSync(`${cd}/config.json`))
+            cpSync(resolve(__dirname, '../../assets/default_config.json'), `${cd}/config.json`);
+        if (!existsSync(`${cd}/wallpapers/default_wallpaper.html`))
+            cpSync(resolve(__dirname, '../../assets/default_wallpaper.html'), `${cd}/wallpapers/default/default_wallpaper.html`);
+        if (!existsSync(`${cd}/wallpapers/default_wallpaper.css`))
+            cpSync(resolve(__dirname, '../../assets/default_wallpaper.css'), `${cd}/wallpapers/default/default_wallpaper.css`);
+        if (!existsSync(`${cd}/wallpapers/default_wallpaper.mp4`))
+            cpSync(resolve(__dirname, '../../assets/default_wallpaper.mp4'), `${cd}/wallpapers/default/default_wallpaper.mp4`);
     }
 }
 
